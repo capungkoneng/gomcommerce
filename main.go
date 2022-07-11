3c748e3d7541e9e1c15b617e73521d1e585b2610
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-07-12 01:37:22 +0700

    cobay deploy

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 
-	db "github.com/capungkoneng/gomcommerce/db/sqlc"
 	api "github.com/capungkoneng/gomcommerce/server"
 	"github.com/capungkoneng/gomcommerce/util"
 	_ "github.com/lib/pq"
@@ -16,13 +14,13 @@ func main() {
 		log.Fatal("cannot load config", err)
 	}
 
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
-	if err != nil {
-		log.Fatal("cannot connect to database", err)
-	}
+	// conn, err := sql.Open(config.DBDriver, config.DBSource)
+	// if err != nil {
+	// 	log.Fatal("cannot connect to database", err)
+	// }
 
-	store := db.NewStore(conn)
-	server, err := api.NewServer(config, store)
+	// store := db.NewStore(conn)
+	server, err := api.NewServer(config)
 	if err != nil {
 		log.Fatal("cannot create server", err)
 	}
