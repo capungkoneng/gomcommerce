be17f464d817ae92c3a270a4e82bc17cc5fa31ca
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-07-12 01:57:53 +0700

    cobay deploy

--- main.go
+++ main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
 
-	api "github.com/capungkoneng/gomcommerce/server"
-	"github.com/capungkoneng/gomcommerce/util"
+	// api "github.com/capungkoneng/gomcommerce/server"
+	// "github.com/capungkoneng/gomcommerce/util"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config", err)
-	}
 
-	// conn, err := sql.Open(config.DBDriver, config.DBSource)
+	port := os.Getenv("PORT")
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/", helloHandler)
+	log.Println("Listing for" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	// config, err := util.LoadConfig(".")
 	// if err != nil {
-	// 	log.Fatal("cannot connect to database", err)
+	// 	log.Fatal("cannot load config", err)
 	// }
 
-	// store := db.NewStore(conn)
-	server, err := api.NewServer(config)
-	if err != nil {
-		log.Fatal("cannot create server", err)
-	}
+	// // conn, err := sql.Open(config.DBDriver, config.DBSource)
+	// // if err != nil {
+	// // 	log.Fatal("cannot connect to database", err)
+	// // }
 
-	err = server.Start(os.Getenv("PORT"))
-	if err != nil {
-		log.Fatal("cannot start server", err)
-	}
+	// // store := db.NewStore(conn)
+	// server, err := api.NewServer(config)
+	// if err != nil {
+	// 	log.Fatal("cannot create server", err)
+	// }
+
+	// err = server.Start(os.Getenv("PORT"))
+	// if err != nil {
+	// 	log.Fatal("cannot start server", err)
+	// }
 }
